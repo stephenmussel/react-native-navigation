a32bc070954f82c7f95cce438c421e1785f5136a
phaydara <phaydara@gmail.com>
2022-07-27 18:54:25 -0500

    added navigation container

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 function App() {
   return (
-    <View style={styles.layout}>
-      <Text style={styles.title}>Hello Again</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.layout}>
+        <Text style={styles.title}>Hello Again</Text>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
 
