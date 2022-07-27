c97ed1e63c1beadc382b9e7d580d4ebf263be19f
phaydara <phaydara@gmail.com>
2022-07-27 18:50:50 -0500

    updated starter code

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.layout}>
+      <Text style={styles.title}>Hello Again</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+export default App;
+
 const styles = StyleSheet.create({
-  container: {
+  layout: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    padding: 8,
+  },
+  title: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
+``
